92d8a9af47bbb667ddc320b9a9d29065e64421ed
AlonsoGD <dani.alonso.garcia@gmail.com>
2018-11-03 15:56:41 -0400

    Created dummy controllers

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var credentials = require('./credentials');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var catalogRouter = require('./routes/catalog'); //Import routes for "catalog" area of site
 
 var app = express();
 
@@ -29,8 +30,10 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+//routes
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/catalog', catalogRouter); // Add catalog routes to middleware chain.
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
